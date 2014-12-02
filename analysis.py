be1537b363135b7292a0a30ab17681a5bb5e76c8
Separ-ation <hunter.j.king@gmail.com>
2014-12-02 18:04:40 -0400

    Update from remote desktop during class

--- analysis.py
+++ analysis.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pylab as pl
-name = "0.1epsilon"
+name = "9hidden2"
 results = np.loadtxt(name+'results.gz')
-indices = np.where(results==2)
+indices = np.where(results==-1)
 results[indices]=1
 length = np.shape(results)[0]
 temp =  results[length-10000:length]
--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import QBot_Threefxn
 import numpy as np
 from os import path
 
-name = "9hidden"
-bot = QBot_Threefxn.QBot_Threefxn(name,50,9,mu=0.7,learning_rate=0.2,epsilon=0.1)
+name = "9hidden2"
+bot = QBot_Threefxn.QBot_Threefxn(name,50,9,mu=0.7,learning_rate=0.2,epsilon=0.01)
 
 ttt = game.game(board_size = 3)
 file_exists = path.isfile(name+'results.gz')
 train_start= 0
-iterations = 1000000
+iterations = 100000
 if file_exists:
     results = np.loadtxt(name+'results.gz')
     iteration_size = np.shape(results)[0]
@@ -20,10 +20,10 @@ else:
 temp = np.zeros((iterations,))
 results = np.append(results,temp)
 for i in range(iterations):
-    if(i%1000==0):
-        print i    
+    if(i%10000==0):
+        print i
     r = ttt.play_with_self(bot)
-    results[train_start+i] = r    
+    results[train_start+i] = r
 bot.save_info()
 np.savetxt(name+'results.gz',results)
 
